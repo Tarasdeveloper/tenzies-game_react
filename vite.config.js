9c59c674c8a083b9e4a13e328c818dcc87280c62
Tarasdeveloper <tomahavki1@gmail.com>
2025-01-10 16:48:05 +0200

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import glob from "glob";
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  root: "src",
+  build: {
+    rollupOptions: {
+      input: glob.sync("./src/*.html"),
+    },
+    outDir: "../dist",
+  },
+  plugins: [injectHTML(), react(), FullReload(["./src/**/**.html"])],
+});
