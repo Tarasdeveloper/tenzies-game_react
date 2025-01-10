a9c7dbabf101c97666ce38eb792be379562cc63a
Tarasdeveloper <tomahavki1@gmail.com>
2025-01-10 17:04:39 +0200

    vite config fix again

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import glob from "glob";
+import { glob } from "glob";
 import injectHTML from "vite-plugin-html-inject";
 import FullReload from "vite-plugin-full-reload";
 
